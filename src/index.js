481eaa52e4c1b342454aa216a2d1abdfe748969a
caio pinho <icandoit2026@gmail.com>
2023-02-06 14:40:20 -0300

    starting project

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// INICIANDO PROJETO
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
